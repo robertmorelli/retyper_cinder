2f934765af070e809a17b79b51f67f2e064e4663
Robert Morelli <robertondino@outlook.com>
2026-06-04 05:42:33 -0600

    use type/ctx helper and patch adder

--- detyper.py
+++ detyper.py
@@ -3,22 +3,20 @@ import sys
 from get_ast_data import get_ast_data, is_const
 from load_source import load_bench
 from anno_remover import remove_annotations
+from update_type_tables import update_type_context_pairs
+from patch_adder import add_patches
 
 source = load_bench(sys.argv[1], sys.argv[2])
 
-roots, types, type_ctxs, components, reads, writes, valid_pair, source_ast = get_ast_data(source)
+roots, types, type_ctxs, components, reads, writes, valid_pair, source_ast, dyn = get_ast_data(source)
 
 all_items_to_remove = set()
 for root in roots:
     all_items_to_remove |= components.get(root) or set()
     all_items_to_remove.add(root)
 
+update_type_context_pairs(dyn, types, type_ctxs, components, reads, writes, all_items_to_remove)
 detyped_ast, table = remove_annotations(source_ast, all_items_to_remove)
-print(ast.unparse(detyped_ast))
+patched_ast = add_patches(detyped_ast, types, type_ctxs, valid_pair)
 
-# get ast data
-# pick anno set from roots
-# remove annotations
-# mod expr tables
-# filter by invalid
-# call patch adder with mod table
+print(ast.unparse(patched_ast))
