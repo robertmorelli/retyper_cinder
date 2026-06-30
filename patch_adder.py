43b1c4ef80468a68a0f7e645c526bfc314ed2554
Robert Morelli <robertondino@outlook.com>
2026-06-29 19:54:36 -0600

    patch adder type erases inflow

--- patch_adder.py
+++ patch_adder.py
@@ -1,18 +1,26 @@
-import ast
+from ast import NodeTransformer, Call, Name, Load, copy_location
 from patch_picker import pick_patch
 
-class PatchAdder(ast.NodeTransformer):
-    def __init__(self, types, type_ctxs, valid_pair):
+class PatchAdder(NodeTransformer):
+    def __init__(self, types, type_ctxs, valid_pair, inflow_nodes):
         self.types = types
         self.type_ctxs = type_ctxs
         self.valid_pair = valid_pair
+        self.inflow_nodes = inflow_nodes
 
     def visit(self, node):
         self.generic_visit(node)
+        result = node
         if (t := self.types.get(node)) and (tc := self.type_ctxs.get(node)):
-            return pick_patch(node, t, tc, self.valid_pair).wrap()
-        return node
+            result = pick_patch(node, t, tc, self.valid_pair).wrap()
+        ctx = getattr(node, "ctx", None)
+        if node in self.inflow_nodes and (ctx is None or isinstance(ctx, Load)):
+            result = copy_location(
+                Call(Name("_cast", Load()), [Name("Any", Load()), result], []),
+                result,
+            )
+        return result
 
-def add_patches(tree, types, type_ctxs, valid_pair):
-    PatchAdder(types, type_ctxs, valid_pair).visit(tree)
+def add_patches(tree, types, type_ctxs, valid_pair, inflow_nodes):
+    PatchAdder(types, type_ctxs, valid_pair, inflow_nodes).visit(tree)
     return tree
