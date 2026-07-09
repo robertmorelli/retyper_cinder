af6b451dada1bb0cd240f430b906b5628550c2e8
Robert Morelli <robertondino@outlook.com>
2026-07-09 16:35:58 -0500

    code style

--- load_source.py
+++ load_source.py
@@ -1,9 +1,9 @@
-import json
+from json import load
 
 def load_bench(bench, variant):
     try:
         with open("data/benchmark_locations.json") as f:
-            sources = json.load(f)
+            sources = load(f)
         path = sources[bench][variant]
         with open(path) as f:
             source = f.read()
