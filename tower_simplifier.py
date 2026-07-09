3c5962ad13303613878c69bc09953b0841dc6ae1
Robert Morelli <robertondino@outlook.com>
2026-07-09 16:31:52 -0500

    add needs exact to tower simplifier

--- tower_simplifier.py
+++ tower_simplifier.py
@@ -1,8 +1,8 @@
-import sys
+from sys import path
 from ast import Call, Name, NodeTransformer
 from patch_picker import pick_patch
 
-sys.path.insert(0, "_cinderx/cinderx/PythonLib")
+path.insert(0, "_cinderx/cinderx/PythonLib")
 from cinderx.compiler.static.types import CType
 
 def extract_coerced(node: Call, constructors):
@@ -19,20 +19,21 @@ def extract_coerced(node: Call, constructors):
     return None
 
 class TowerSimplifier(NodeTransformer):
-    def __init__(self, constructors, valid_pair, types, type_ctxs):
+    def __init__(self, constructors, valid_pair, types, type_ctxs, needs_exact):
         self.constructors = constructors
         self.valid_pair = valid_pair
         self.types = types
         self.type_ctxs = type_ctxs
+        self.needs_exact = needs_exact
 
     def visit_Call(self, node):
         if inner := extract_coerced(node, self.constructors):
             tc = self.type_ctxs.get(node)
             t = self.types.get(inner)
-            node = pick_patch(inner, t, tc, self.valid_pair).wrap()
+            node = pick_patch(inner, t, tc, self.valid_pair, self.needs_exact).wrap()
         self.generic_visit(node)
         return node
 
-def simplify_coercions(tree, constructors, valid_pair, types, type_ctxs):
-    TowerSimplifier(constructors, valid_pair, types, type_ctxs).visit(tree)
+def simplify_coercions(tree, constructors, valid_pair, types, type_ctxs, needs_exact):
+    TowerSimplifier(constructors, valid_pair, types, type_ctxs, needs_exact).visit(tree)
     return tree
